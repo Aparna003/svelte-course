11b4e37846f6e53ad787ad0745e62179b440890c
Aparna003 <aparnashankar004@gmail.com>
2021-04-02 23:54:55 +0530

    two way binding and multiple components done

---
+++ src/App.svelte
@@ -0,0 +1,55 @@
+<script>
+	import ContactCard from "./ContactCard.svelte";
+	export let name;
+	let age = 20;
+
+	// let uppercaseName; is not required
+	$: uppercaseName = name.toUpperCase();
+	$: console.log(name);
+	function increaseAge() {
+		age += 1;
+	}
+	$: if (name === "Bangy") {
+		age=19;
+	}
+	function changeName() {
+		name = "Bangy"; // even though name is changed, the uppercase is still maintained
+	}
+	function nameInput(event){
+	const newName = event.target.value;
+	name=newName;
+	}
+</script>
+
+<main>
+	<h1>Hello {uppercaseName}!<br /> My age is {age}</h1>
+	<!-- "" can and neednt be used for attributes if {} are used -->
+	<button on:click={increaseAge}>Change Age</button>
+	<!-- <input type="text" value="{name}" on:input={nameInput} /> -->
+  
+	<!-- 2 way bind shortcut -->
+	  <input type="text" bind:value="{name}" />
+      <ContactCard/>
+	</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		/* text-transform: uppercase; */
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
---
+++ src/ContactCard.svelte
@@ -0,0 +1,61 @@
+<script>
+</script>
+
+
+
+<style scoped>
+.contact-card{
+    box-shadow:  0 2px 8px rgba(0,0,0,0.26);
+    max-width: 30rem;
+    border-radius: 5px;
+    margin: 1rem 0;
+}
+header{
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+}
+.thumb{
+    width:33%;
+    height: 100%;
+}
+img{
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+}
+.user-data{
+    width: 80%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+}
+h1{
+    font-size: 1.25rem;
+    font-family: "Roboto Slab",sans-serif;
+}
+h2{
+    font-size: 1rem;
+    font-weight: normal;
+    color: #5a5a5a;
+}
+.desc{
+    border-top:1px solid #ccc;
+    padding: 1rem;
+}
+</style>
+
+<div class="contact-card">
+    <header>
+        <div class="thumb">
+            <img src="" alt="" />
+        </div>
+        <div class="user-data">
+            <h1>User Name</h1>
+            <h2>Job title</h2>
+        </div>
+    </header>
+    <div>
+        <p class="desc"> short description </p>
+    </div>
+</div>
