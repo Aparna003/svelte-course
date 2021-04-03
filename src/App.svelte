298359d9aa1bf834394f076bb5240fd64473008a
Aparna003 <aparnashankar004@gmail.com>
2021-04-03 20:28:47 +0530

    if else if types

--- src/App.svelte
+++ src/App.svelte
@@ -5,61 +5,78 @@
 	let title="";
 	let des="";
 	let image="https://source.unsplash.com/random";
-
-	// let uppercaseName; is not required
-	$: uppercaseName = name.toUpperCase();
-	$: console.log(name);
-	function increaseAge() {
-		age += 1;
-	}
-	$: if (name === "Bangy") {
-		age=19;
-	}
-	function changeName() {
-		name = "Bangy"; // even though name is changed, the uppercase is still maintained
-	}
-	function nameInput(event){
-	const newName = event.target.value;
-	name=newName;
+    let formState='empty';
+	// // let uppercaseName; is not required
+	// $: uppercaseName = name.toUpperCase();
+	// $: console.log(name);
+	// function increaseAge() {
+	// 	age += 1;
+	// }
+	// $: if (name === "Bangy") {
+	// 	age=19;
+	// }
+	// function changeName() {
+	// 	name = "Bangy"; // even though name is changed, the uppercase is still maintained
+	// }
+	// function nameInput(event){
+	// const newName = event.target.value;
+	// name=newName;
+	// }
+	function addContact(){
+		if(name.trim().length==0 ||
+		title.trim().length==0 ||
+		image.trim().length==0 ||
+		des.trim().length==0
+		){
+           formState="invalid";
+		   return;
+		}
+		formState='done';
 	}
 </script>
 
-<main>
-	<h1>Hello {uppercaseName}!<br /> My age is {age}</h1>
-	<!-- "" can and neednt be used for attributes if {} are used -->
-	<button on:click={increaseAge}>Change Age</button>
-	<!-- <input type="text" value="{name}" on:input={nameInput} /> -->
-  
-	<!-- 2 way bind shortcut -->
-	  <input type="text" bind:value="{name}" />
-	  <input type="text" bind:value="{title}" />
-	  <input type="text" bind:value="{image}" />
-	  <textarea rows="3" type="text" bind:value="{des}" />
-      <ContactCard 
-	  userName={name} 
-	  jobTitle={title} 
-	  description={des} 
-	  userImage={image} />
-	</main>
-
+<div class="form">
+	<div class="form-control">
+      <label for="userName">User Name</label>
+	  <input type="text" bind:value={name} id="userName" />
+	</div>
+	<div class="form-control">
+		<label for="userName">Job title</label>
+		<input type="text" bind:value={title} id="jobTitle" />
+	</div>
+	<div class="form-control">
+		<label for="userName">Image URL</label>
+		<input type="text" bind:value={image} id="image" />
+	</div>
+	<div class="form-control">
+		<label for="userName">Description</label>
+		<textarea rows="3" type="text" bind:value={des} id="desc" />
+	</div>
+</div>
+<button on:click="{addContact}">Add contact card</button>
+{#if formState==="done"
+}
+<ContactCard userName={name} 
+jobTitle={title} 
+description={des} 
+userImage={image}/>
+{:else if formState==="invalid"}
+<p>Invalid input</p>
+{:else}
+<p>Please fill all the fields and submit!</p>
+<!-- {#if formState==="invalid"} -->
+{/if}
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
+	.form {
+		width: 30rem;
+		max-width: 100%;
 	}
-
-	h1 {
-		color: #ff3e00;
-		/* text-transform: uppercase; */
-		font-size: 4em;
-		font-weight: 100;
+	button{
+	background-color: #e40763;
+	color: aliceblue;
+	outline: none;
+	border-radius: 0.5em;
+	padding: 0.5rem;
 	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+	
 </style>
